8709e1e58c8abe20e264c3b5e67a5c9b09040028
Benedikt Lüken-Winkels <s4beluek@uni-trier.de>
2020-01-09 14:19:09 +0100

    Add Removebutton

--- src/main/react/examanagerii/src/components/results/StudentResult.js
+++ src/main/react/examanagerii/src/components/results/StudentResult.js
@@ -30,17 +30,28 @@ const StudentResult = ({student, exam, result, notify}) => {
     return (
         <div className={"w3-display-container w3-padding"} style={{height: 30}}>
             <div className={"w3-display-left"}>{student.firstname} {student.lastname}</div>
-            {result && result.totalReached ?
+            {result && result.totalReached !== null ?
                 <div className={"w3-display-middle"}>{result.totalReached} Punkte</div>
                 :<div className={"w3-display-middle"}>Kein Ergebnis</div>
             }
-            <Button
-                className={"w3-display-right"}
-                label={"Eintragen"}
-                onClick={() => setShowModal(true)}
-                nomargin={true}
-                disabled={!exam}
-            />
+            <div className={"w3-display-right"}>
+                <Button
+                    className={"w3-small"}
+                    label={"Eintragen"}
+                    onClick={() => setShowModal(true)}
+                    nomargin={true}
+                    disabled={!exam}
+                />
+                &nbsp;
+                <Button
+                    className={"w3-small w3-orange"}
+                    label={<span>&times;</span>}
+                    onClick={() => setShowModal(true)}
+                    nomargin={true}
+                    disabled={!result || result.totalReached === null}
+                />
+            </div>
+
             {showModal &&
                 <StudentResultModal
                     student={student}
